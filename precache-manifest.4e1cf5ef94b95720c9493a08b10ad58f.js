1f54b45b2fd3a1fc001ee97111eee8f831ad276a
dyridantudk <dyridantudK@gmail.com>
2020-09-30 11:17:58 +0600

    Updates

---
+++ precache-manifest.4e1cf5ef94b95720c9493a08b10ad58f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f742d69c02be1abf1de64c3975abd87",
+    "url": "/IDK/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/IDK/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4a69d7a8f2fb563bbec5",
+    "url": "/IDK/static/css/main.614d3eee.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/IDK/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IDK/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a69d7a8f2fb563bbec5",
+    "url": "/IDK/static/js/main.d0d2f931.chunk.js"
+  },
+  {
+    "revision": "bc0d806bef5b1d3c947b",
+    "url": "/IDK/static/js/runtime-main.e2ddfdf7.js"
+  },
+  {
+    "revision": "61be1af1af2ed8629c1595102f8e4fe5",
+    "url": "/IDK/static/media/SansSerifShaded.61be1af1.woff"
+  }
+]);
